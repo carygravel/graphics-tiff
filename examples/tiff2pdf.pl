17eac5de5a707b87eeed4c1a702197988f898ade
Jeffrey Ratcliffe <4869402+carygravel@users.noreply.github.com>
2017-03-08 14:12:44 +0100

    + t2p_compose_pdf_page()

--- examples/tiff2pdf.pl
+++ examples/tiff2pdf.pl
@@ -2414,6 +2414,242 @@ sub t2p_write_pdf_pages {
     return length $buffer;
 }
 
+# This function composes the page size and image and tile locations on a page.
+
+sub t2p_compose_pdf_page {
+    my ($t2p) = @_;
+
+    $t2p->{pdf_xres} = $t2p->{tiff_xres};
+    $t2p->{pdf_yres} = $t2p->{tiff_yres};
+    if ( $t2p->{pdf_overrideres} ) {
+        $t2p->{pdf_xres} = $t2p->{pdf_defaultxres};
+        $t2p->{pdf_yres} = $t2p->{pdf_defaultyres};
+    }
+    if ( $t2p->{pdf_xres} == 0.0 ) {
+        $t2p->{pdf_xres} = $t2p->{pdf_defaultxres};
+    }
+    if ( $t2p->{pdf_yres} == 0.0 ) {
+        $t2p->{pdf_yres} = $t2p->{pdf_defaultyres};
+    }
+    if ( $t2p->{pdf_image_fillpage} ) {
+        my $width_ratio  = $t2p->{pdf_defaultpagewidth} / $t2p->{tiff_width};
+        my $length_ratio = $t2p->{pdf_defaultpagelength} / $t2p->{tiff_length};
+        if ( $width_ratio < $length_ratio ) {
+            $t2p->{pdf_imagewidth}  = $t2p->{pdf_defaultpagewidth};
+            $t2p->{pdf_imagelength} = $t2p->{tiff_length} * $width_ratio;
+        }
+        else {
+            $t2p->{pdf_imagewidth}  = $t2p->{tiff_width} * $length_ratio;
+            $t2p->{pdf_imagelength} = $t2p->{pdf_defaultpagelength};
+        }
+    }
+    elsif (
+        $t2p->{tiff_resunit} != RESUNIT_CENTIMETER    # RESUNIT_NONE and
+        && $t2p->{tiff_resunit} != RESUNIT_INCH       # other cases
+      )
+    {
+        $t2p->{pdf_imagewidth}  = $t2p->{tiff_width} / $t2p->{pdf_xres};
+        $t2p->{pdf_imagelength} = $t2p->{tiff_length} / $t2p->{pdf_yres};
+    }
+    else {
+        $t2p->{pdf_imagewidth} =
+          $t2p->{tiff_width} * $PS_UNIT_SIZE / $t2p->{pdf_xres};
+        $t2p->{pdf_imagelength} =
+          $t2p->{tiff_length} * $PS_UNIT_SIZE / $t2p->{pdf_yres};
+    }
+    if ( $t2p->{pdf_overridepagesize} != 0 ) {
+        $t2p->{pdf_pagewidth}  = $t2p->{pdf_defaultpagewidth};
+        $t2p->{pdf_pagelength} = $t2p->{pdf_defaultpagelength};
+    }
+    else {
+        $t2p->{pdf_pagewidth}  = $t2p->{pdf_imagewidth};
+        $t2p->{pdf_pagelength} = $t2p->{pdf_imagelength};
+    }
+    $t2p->{pdf_mediabox}{x1} = 0.0;
+    $t2p->{pdf_mediabox}{y1} = 0.0;
+    $t2p->{pdf_mediabox}{x2} = $t2p->{pdf_pagewidth};
+    $t2p->{pdf_mediabox}{y2} = $t2p->{pdf_pagelength};
+    $t2p->{pdf_imagebox}{x1} = 0.0;
+    $t2p->{pdf_imagebox}{y1} = 0.0;
+    $t2p->{pdf_imagebox}{x2} = $t2p->{pdf_imagewidth};
+    $t2p->{pdf_imagebox}{y2} = $t2p->{pdf_imagelength};
+
+    if ( $t2p->{pdf_overridepagesize} != 0 ) {
+        $t2p->{pdf_imagebox}{x1} +=
+          ( $t2p->{pdf_pagewidth} - $t2p->{pdf_imagewidth} ) / 2.0;
+        $t2p->{pdf_imagebox}{y1} +=
+          ( $t2p->{pdf_pagelength} - $t2p->{pdf_imagelength} ) / 2.0;
+        $t2p->{pdf_imagebox}{x2} +=
+          ( $t2p->{pdf_pagewidth} - $t2p->{pdf_imagewidth} ) / 2.0;
+        $t2p->{pdf_imagebox}{y2} +=
+          ( $t2p->{pdf_pagelength} - $t2p->{pdf_imagelength} ) / 2.0;
+    }
+    if ( $t2p->{tiff_orientation} > 4 ) {
+        my $f = $t2p->{pdf_mediabox}{x2};
+        $t2p->{pdf_mediabox}{x2} = $t2p->{pdf_mediabox}{y2};
+        $t2p->{pdf_mediabox}{y2} = $f;
+    }
+    my $tiles;
+    if ( $t2p->{tiff_tiles}[ $t2p->{pdf_page} ]{tiles_tilecount} == 0 ) {
+        t2p_compose_pdf_page_orient( $t2p->{pdf_imagebox},
+            $t2p->{tiff_orientation} );
+        return;
+    }
+    else {
+        my $tilewidth = $t2p->{tiff_tiles}[ $t2p->{pdf_page} ]{tiles_tilewidth};
+        my $tilelength =
+          $t2p->{tiff_tiles}[ $t2p->{pdf_page} ]{tiles_tilelength};
+        my $tilecountx = ( $t2p->{tiff_width} + $tilewidth - 1 ) / $tilewidth;
+        $t2p->{tiff_tiles}[ $t2p->{pdf_page} ]{tiles_tilecountx} = $tilecountx;
+        my $tilecounty =
+          ( $t2p->{tiff_length} + $tilelength - 1 ) / $tilelength;
+        $t2p->{tiff_tiles}[ $t2p->{pdf_page} ]{tiles_tilecounty} = $tilecounty;
+        $t2p->{tiff_tiles}[ $t2p->{pdf_page} ]{tiles_edgetilewidth} =
+          $t2p->{tiff_width} % {$tilewidth};
+        $t2p->{tiff_tiles}[ $t2p->{pdf_page} ]{tiles_edgetilelength} =
+          $t2p->{tiff_length} % $tilelength;
+        $tiles = $t2p->{tiff_tiles}[ $t2p->{pdf_page} ]{tiles_tiles};
+
+        for my $i2 ( 0 .. $tilecounty - 2 ) {
+            for my $i ( 0 .. $tilecountx - 2 ) {
+                my $boxp = $tiles->[ $i2 * $tilecountx + $i ]{tile_box};
+                $boxp->{x1} =
+                  $t2p->{pdf_imagebox}{x1} +
+                  $t2p->{pdf_imagewidth} * $i * $tilewidth / $t2p->{tiff_width};
+                $boxp->{x2} =
+                  $t2p->{pdf_imagebox}{x1} +
+                  $t2p->{pdf_imagewidth} *
+                  ( $i + 1 ) *
+                  $tilewidth /
+                  $t2p->{tiff_width};
+                $boxp->{y1} =
+                  $t2p->{pdf_imagebox}{y2} -
+                  $t2p->{pdf_imagelength} *
+                  ( $i2 + 1 ) *
+                  $tilelength /
+                  $t2p->{tiff_length};
+                $boxp->{y2} =
+                  $t2p->{pdf_imagebox}{y2} -
+                  $t2p->{pdf_imagelength} *
+                  $i2 * $tilelength /
+                  $t2p->{tiff_length};
+            }
+            my $boxp =
+              $tiles->[ $i2 * $tilecountx + $tilecountx - 1 ]{tile_box};
+            $boxp->{x1} =
+              $t2p->{pdf_imagebox}{x1} +
+              $t2p->{pdf_imagewidth} * $tilecountx -
+              1 * $tilewidth / $t2p->{tiff_width};
+            $boxp->{x2} = $t2p->{pdf_imagebox}{x2};
+            $boxp->{y1} =
+              $t2p->{pdf_imagebox}{y2} -
+              $t2p->{pdf_imagelength} *
+              ( $i2 + 1 ) *
+              $tilelength /
+              $t2p->{tiff_length};
+            $boxp->{y2} =
+              $t2p->{pdf_imagebox}{y2} -
+              $t2p->{pdf_imagelength} *
+              $i2 *
+              {$tilelength} /
+              $t2p->{tiff_length};
+        }
+        for my $i ( 0 .. $tilecountx - 2 ) {
+            my $boxp =
+              $tiles->[ ( $tilecounty - 1 ) * $tilecountx + $i ]{tile_box};
+            $boxp->{x1} =
+              $t2p->{pdf_imagebox}{x1} +
+              $t2p->{pdf_imagewidth} * $i * $tilewidth / $t2p->{tiff_width};
+            $boxp->{x2} =
+              $t2p->{pdf_imagebox}{x1} +
+              $t2p->{pdf_imagewidth} *
+              ( $i + 1 ) *
+              $tilewidth /
+              $t2p->{tiff_width};
+            $boxp->{y1} = $t2p->{pdf_imagebox}{y1};
+            $boxp->{y2} =
+              $t2p->{pdf_imagebox}{y2} -
+              $t2p->{pdf_imagelength} *
+              ( $tilecounty - 1 ) *
+              $tilelength /
+              $t2p->{tiff_length};
+        }
+        my $boxp =
+          $tiles->[ ( $tilecounty - 1 ) * $tilecountx + $tilecountx - 1 ]
+          {tile_box};
+        $boxp->{x1} =
+          $t2p->{pdf_imagebox}{x1} +
+          $t2p->{pdf_imagewidth} *
+          ( $tilecountx - 1 ) *
+          $tilewidth /
+          $t2p->{tiff_width};
+        $boxp->{x2} = $t2p->{pdf_imagebox}{x2};
+        $boxp->{y1} = $t2p->{pdf_imagebox}{y1};
+        $boxp->{y2} =
+          $t2p->{pdf_imagebox}{y2} -
+          $t2p->{pdf_imagelength} *
+          ( $tilecounty - 1 ) *
+          $tilelength /
+          $t2p->{tiff_length};
+    }
+    if ( $t2p->{tiff_orientation} == 0 || $t2p->{tiff_orientation} == 1 ) {
+        for my $i (
+            0 .. $t2p->{tiff_tiles}[ $t2p->{pdf_page} ]{tiles_tilecount} - 1 )
+        {
+            t2p_compose_pdf_page_orient( $tiles->[$i]{tile_box}, 0 );
+        }
+        return;
+    }
+    for
+      my $i ( 0 .. $t2p->{tiff_tiles}[ $t2p->{pdf_page} ]{tiles_tilecount} - 1 )
+    {
+        my $boxp = $tiles->[$i]{tile_box};
+        $boxp->{x1} -= $t2p->{pdf_imagebox}{x1};
+        $boxp->{x2} -= $t2p->{pdf_imagebox}{x1};
+        $boxp->{y1} -= $t2p->{pdf_imagebox}{y1};
+        $boxp->{y2} -= $t2p->{pdf_imagebox}{y1};
+        if ( $t2p->{tiff_orientation} == 2 || $t2p->{tiff_orientation} == 3 ) {
+            $boxp->{x1} =
+              $t2p->{pdf_imagebox}{x2} - $t2p->{pdf_imagebox}{x1} - $boxp->{x1};
+            $boxp->{x2} =
+              $t2p->{pdf_imagebox}{x2} - $t2p->{pdf_imagebox}{x1} - $boxp->{x2};
+        }
+        if ( $t2p->{tiff_orientation} == 3 || $t2p->{tiff_orientation} == 4 ) {
+            $boxp->{y1} =
+              $t2p->{pdf_imagebox}{y2} - $t2p->{pdf_imagebox}{y1} - $boxp->{y1};
+            $boxp->{y2} =
+              $t2p->{pdf_imagebox}{y2} - $t2p->{pdf_imagebox}{y1} - $boxp->{y2};
+        }
+        if ( $t2p->{tiff_orientation} == 8 || $t2p->{tiff_orientation} == 5 ) {
+            $boxp->{y1} =
+              $t2p->{pdf_imagebox}{y2} - $t2p->{pdf_imagebox}{y1} - $boxp->{y1};
+            $boxp->{y2} =
+              $t2p->{pdf_imagebox}{y2} - $t2p->{pdf_imagebox}{y1} - $boxp->{y2};
+        }
+        if ( $t2p->{tiff_orientation} == 5 || $t2p->{tiff_orientation} == 6 ) {
+            $boxp->{x1} =
+              $t2p->{pdf_imagebox}{x2} - $t2p->{pdf_imagebox}{x1} - $boxp->{x1};
+            $boxp->{x2} =
+              $t2p->{pdf_imagebox}{x2} - $t2p->{pdf_imagebox}{x1} - $boxp->{x2};
+        }
+        if ( $t2p->{tiff_orientation} > 4 ) {
+            my $f = $boxp->{x1};
+            $boxp->{x1} = $boxp->{y1};
+            $boxp->{y1} = $f;
+            $f          = $boxp->{x2};
+            $boxp->{x2} = $boxp->{y2};
+            $boxp->{y2} = $f;
+            t2p_compose_pdf_page_orient_flip( $boxp, $t2p->{tiff_orientation} );
+        }
+        else {
+            t2p_compose_pdf_page_orient( $boxp, $t2p->{tiff_orientation} );
+        }
+
+    }
+
+    return;
+}
+
 # This function writes a PDF to a file given a pointer to a TIFF.
 
 # The idea with using a TIFF* as output for a PDF file is that the file
