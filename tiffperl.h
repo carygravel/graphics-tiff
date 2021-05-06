c47fba777e6d5f0d02b702693561956f2cfef58a
Phil Perry <phil4597@catskilltech.com>
2021-05-06 19:39:51 -0400

    Rename define guard _TIFFPERL_H_ -> TIFFPERL_H_ to satisfy linter

--- tiffperl.h
+++ tiffperl.h
@@ -6,10 +6,10 @@
  * at your option, any later version of Perl 5 you may have available.
  */
 
-#ifndef _TIFFPERL_H_
-#define _TIFFPERL_H_
+#ifndef TIFFPERL_H_
+#define TIFFPERL_H_
 
 /* Include all of libtiff's headers for internal consistency */
 #include <tiffio.h>
 
-#endif /* _TIFFPERL_H_ */
+#endif  // TIFFPERL_H_
