35c5cafe542bfe13bc7e51b50ae610d82035577e
Jeffrey Ratcliffe <4869402+carygravel@users.noreply.github.com>
2021-05-31 00:00:00 +0000

    Bumped version to 14

--- lib/Graphics/TIFF.pm
+++ lib/Graphics/TIFF.pm
@@ -208,7 +208,7 @@ our %EXPORT_TAGS = (
 
 our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
 
-our $VERSION = 13;
+our $VERSION = 14;
 
 require XSLoader;
 XSLoader::load( 'Graphics::TIFF', $VERSION );
@@ -246,7 +246,7 @@ Graphics::TIFF - Perl extension for the libtiff library
 
 =head1 VERSION
 
-13
+14
 
 =head1 SYNOPSIS
 
